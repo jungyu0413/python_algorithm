0bb44a1f0449f342fe9f834915840189fb4d18c3
Jungyu Lee <71767623+jungyu0413@users.noreply.github.com>
2025-01-20 12:48:54 +0900

    Update sol3.py

--- sol3.py
+++ sol3.py
@@ -5,6 +5,7 @@ class Solution:
         
         while fast and fast.next:
             fast = fast.next.next
+            # reverse linkedlist
             rev, rev.next, slow = slow, rev, slow.next
         if fast:
             slow = slow.next
