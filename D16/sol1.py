343f6108da549bb7238acfcb5e239716a1ea18af
Jungyu Lee <71767623+jungyu0413@users.noreply.github.com>
2025-02-05 17:35:13 +0900

    Update sol1.py

--- D16/sol1.py
+++ D16/sol1.py
@@ -1,9 +1,40 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        a = self.toList(self.reverseList(l1))
+        b = self.toList(self.reverseList(l2))
+
+        resultStr = int(''.join(str(e) for e in a)) + int(''.join(str(e) for e in b))
+
+        return self.toReversedLinkedList(str(resultStr))
+
+    
+    def toReversedLinkedList(self, result: ListNode) -> ListNode:
+        prev : ListNode = None
+        for r in result:
+            node = ListNode(int(r))
+            node.next = prev
+            prev = node
+        return node
+
+    def toList(self, node: ListNode) -> ListNode:
+        list: List = []
+        while node:
+            list.append(node.val)
+            node = node.next
+        return list
+
+
+    def reverseList(self, head: ListNode) -> ListNode:
         node, prev = head, None
 
         while node:
             next, node.next = node.next, prev
             prev, node = node, next
-
+        
         return prev
+        
