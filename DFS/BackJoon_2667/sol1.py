810e1abb347491d91364cfba227fc2389802c64c
Jungyu Lee <71767623+jungyu0413@users.noreply.github.com>
2025-04-07 16:01:17 +0900

    Update sol1.py

--- DFS/BackJoon_2667/sol1.py
+++ DFS/BackJoon_2667/sol1.py
@@ -3,39 +3,37 @@ from collections import deque
 # input = sys.stdin.readline
 # import sys
 sys.stdin = open('C:\\Users\\Lee\\Desktop\\algorithm\\BFS\\BackJoon_1926\\input.txt', 'r')
-n, m = map(int, input().split())
-map = [list(map(int, input().split())) for _ in range(n)]
-ch = [[False] * m for _ in range(n)]
+import sys
+# input = sys.stdin.readline
 
-dy = [0, 1, 0, -1]
-dx = [1, 0, -1, 0]
+N = int(input())
+map = [list(map(int, input().strip())) for _ in range(N)]
+chk = [[False] * N for _ in range(N)]
+result = []
+each = 0
+dy = [0,1,0,-1]
+dx = [1,0,-1,0]
 
-def bfs(y, x):
-    q = deque()
-    q.append((y, x))
-    r = 1
-    while q:
-        ey, ex = q.popleft()
-        for k in range(4):
-            ny = ey + dy[k]
-            nx = ex + dx[k]
+def dfs(y, x):
+    global each
+    each += 1
+    for k in range(4):
+        ny = y + dy[k]
+        nx = x + dx[k]
+        if 0<=ny<N and 0<=nx<N:
+            if map[ny][nx] == 1 and chk[ny][nx] == False:
+                chk[ny][nx] = True
+                dfs(ny, nx)
 
-            if 0<=ny<n and 0<=nx<m:
-                if map[ny][nx] == 1 and ch[ny][nx] == False:
-                    ch[ny][nx] = True
-                    r += 1
-                    q.append((ny, nx))
+for j in range(N):
+    for i in range(N):
+        if map[j][i] == 1 and chk[j][i] == False:
+            chk[j][i] = True
+            each = 0
+            dfs(j,i)
+            result.append(each)
 
-    return r
-
-cnt = 0
-maxv = 0
-for j in range(n):
-    for i in range(m):
-        if map[j][i] == 1 and ch[j][i] == False:
-            ch[j][i] = True
-            cnt += 1
-            maxv = max(maxv, bfs(j, i))
-
-print(cnt)
-print(maxv)
+result.sort()
+print(len(result))
+for i in result:
+    print(i)
